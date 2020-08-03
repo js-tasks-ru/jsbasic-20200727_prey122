363b0fd8033f462a783552e99815a79e430f25fa
Nikita <prey122@gmail.com>
2020-08-03 16:42:55 +0300

    spam

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,9 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
-  // ваш код...
+    str = str.toUpperCase();
+    if(str.indexOf('1XBET') !== -1 || str.indexOf('XXX') !== -1){
+        return true;
+    }else 
+    return false;
 }
