e1c25f8d7b1588b4412802941a0ebdb07b2d60ee
Nikita <prey122@gmail.com>
2020-08-07 16:39:01 +0300

    sumSalary

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -5,4 +5,14 @@
  */
 function sumSalary(salaries) {
   // ваш код...
+  let sumSalary = 0;
+  for(let key in salaries){
+    if (!isNaN(salaries[key])){
+      sumSalary += salaries[key];
+    }
+  }
+  if( sumSalary === 0){
+    return 0;
+  }
+  return sumSalary;
 }
