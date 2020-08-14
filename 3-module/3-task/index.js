c74136fbfd1728797ba8550e5501b57b6fe42cde
Nikita <prey122@gmail.com>
2020-08-14 19:40:52 +0300

    split and join

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -4,4 +4,15 @@
  */
 function camelize(str) {
   // ваш код...
+  let target = '-';
+  let pos = -1;
+  let newArr = str;
+  newArr = newArr.split('');
+  while((pos = newArr.indexOf(target,pos + 1)) != -1){
+      pos;
+      newArr.splice(pos,2,newArr[++pos].toUpperCase());
+      
+  };
+  return newArr = newArr.join('');
+
 }
