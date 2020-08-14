33fb76cba29adc1c8c1536b393ccff8573197966
Nikita <prey122@gmail.com>
2020-08-15 00:25:17 +0300

    showSalary

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -6,4 +6,11 @@
  */
 function showSalary(users, age) {
   // ваш код...
+  let someUser;
+  someUser = users.filter(item => item.age <= age);
+  let newArray = someUser.map( item => item.name + ", " + item.balance + '\n');
+  newArray = newArray.join('');
+  let minus_n = newArray.length;
+  minus_n--;
+  return newArray.slice(0, minus_n);
 }
