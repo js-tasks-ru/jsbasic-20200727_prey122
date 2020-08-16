245f047c2bc73796a43ab8c119f4e5a114638dc9
Nikita <prey122@gmail.com>
2020-08-16 12:14:53 +0300

    min max

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -5,4 +5,14 @@
  */
 function getMinMax(str) {
   // ваш код...
+  str = str.split(' ');
+   str = str.map(item => item.split(','));
+   str = str = str.map(item => item.join(' '));
+   str = str.join(' ');
+   str =  str.split(' ');
+    str = str.filter(item => !isNaN(item));
+   let result ={};
+   result.max = Math.max(...str);
+   result.min = Math.min(...str);
+   return result;
 }
