488f4337e39f184d93bfab61076df0aa454e3a20
Nikita <prey122@gmail.com>
2020-08-21 14:46:20 +0300

    makeDiagonRed

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,12 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+    
+    // table.rows[0].style.backgroundColor = 'red';
+   for(let i = 0; i < table.rows.length; i++){
+
+       table.rows[i].cells[i].style.backgroundColor= 'red';
+
+   }
+   return console.log(table.rows.length);
 }
