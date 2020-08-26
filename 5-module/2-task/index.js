185d8f890c9ff7380bd0d88065ce7190c2c2f372
Nikita <prey122@gmail.com>
2020-08-26 23:49:14 +0300

    toggleText

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,11 @@
 function toggleText() {
   // ваш код...
+  document.querySelector('.toggle-text-button').addEventListener('click', function(){
+    let text = document.getElementById('text');
+    if(text.hasAttribute('hidden')){
+      text.removeAttribute('hidden')
+    }else
+    text.setAttribute('hidden', '');
+    
+  })
 }
