803d21ffb5c6cc642efbb276ca01a2b7f0d60f4d
Nikita <prey122@gmail.com>
2020-08-26 22:40:13 +0300

    rabota nad oshibkami i nov modul

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -7,21 +7,24 @@ function highlight(table) {
     for(let i = 0; i < table.rows.length; i++){
         for (j=0; j < table.rows[i].cells.length; j++){
             if(table.rows[i].cells[j].dataset.available == 'true'){
-                table.rows[i].cells[j].setAttribute('class', 'available');
+                table.rows[i].classList.add( 'available');
             }
-            if(table.rows[i].cells[j].dataset.available == 'false')
-            { table.rows[i].cells[j].setAttribute('class', 'unavailable');}
-            if(table.rows[i].cells[j].dataset.available == undefined){
-                table.rows[i].cells[j].setAttribute('hidden','');
-            }
-            if(table.rows[i].cells[j].innerHTML === 'm'){
-                table.rows[i].cells[j].setAttribute('class','male');
+            if(table.rows[i].cells[j].dataset.available == 'false'){
+                 table.rows[i].classList.add( 'unavailable');
+                }
+            if(table.rows[i].cells[j].dataset.available !== undefined){
+                table.rows[i].removeAttribute('hidden'); 
+            }else{table.rows[i].setAttribute('hidden','')}
+            
+            if(table.rows[i].cells[j].innerHTML == 'm'){
+                console.log(table.rows[i]);
+                table.rows[i].classList.add('male');
             }
             if(table.rows[i].cells[j].innerHTML === 'f'){
-                table.rows[i].cells[j].setAttribute('class','female');
+                table.rows[i].classList.add('female');
             }
             if(table.rows[i].cells[j].innerHTML < 18){
-                table.rows[i].cells[j].style.textDecoration = 'line-through';
+                table.rows[i].style.textDecoration = 'line-through';
             }
         }
     }
